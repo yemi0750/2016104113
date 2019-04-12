3073052fb81d09ba91ca3d7f1594777abc421351
yemi <yemi___@khu.ac.kr>
2019-04-12 10:27:53 +0900

    0 to 1

--- googoo.cpp
+++ googoo.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
 	cout << "구구단 입니다" << endl;
-	for(int i=0;i<9;i++){
-		for(int j=0;j<9;j++){
+	for(int i=1;i<=9;i++){
+		for(int j=1;j<=9;j++){
 			cout<<i<<"*"<<j<<"="<<i*j<<endl;
 		}
 	}
